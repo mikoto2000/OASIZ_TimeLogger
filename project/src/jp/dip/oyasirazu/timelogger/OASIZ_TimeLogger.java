917fc3734f6c5bc01cfb57b51e690c6cc50819fd
mikoto2000 <mikoto2000@gmail.com>
2011-07-03 01:19:19 +0900

    first commit

---
+++ project/src/jp/dip/oyasirazu/timelogger/OASIZ_TimeLogger.java
@@ -0,0 +1,13 @@
+package jp.dip.oyasirazu.timelogger;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OASIZ_TimeLogger extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

2dec58536e30123703cdb7c1c3721a9e6151802d
mikoto <mikoto2000@gmail.com>
2011-08-12 00:31:36 +0900

    Work, Worklistadapter にヘッダを追加しました。

--- project/src/jp/dip/oyasirazu/timelogger/Work.java
+++ project/src/jp/dip/oyasirazu/timelogger/Work.java
@@ -1,3 +1,29 @@
+/**
+ * Work.java
+ * 
+ * The MIT License
+ * 
+ * Copyright (c) 2011 mikoto2000<mikoto2000@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package jp.dip.oyasirazu.timelogger;
 
 import java.util.Date;
@@ -5,12 +31,12 @@ import java.util.Date;
 public class Work {
     private String mName;
     private Date mStartDate;
-    private long mSpentTime;
+    private Date mSpentDate;
     
-    public Work(String name, Date startDate, long spentTime) {
+    public Work(String name, Date startDate, Date spentTime) {
         this.mName = name;
         this.mStartDate = startDate;
-        this.mSpentTime = spentTime;
+        this.mSpentDate = spentTime;
     }
     
     public String getName() {
@@ -29,11 +55,11 @@ public class Work {
         this.mStartDate = startTime;
     }
     
-    public long getSpentTime() {
-        return mSpentTime;
+    public Date getSpentDate() {
+        return mSpentDate;
     }
     
-    public void setSpentTime(long spentTime) {
-        this.mSpentTime = spentTime;
+    public void setSpentTime(Date spentDate) {
+        this.mSpentDate = spentDate;
     }
 }

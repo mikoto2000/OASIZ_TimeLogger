80f8547227411880beb79747aba06486a8632b39
mikoto <mikoto2000@gmail.com>
2011-08-29 03:33:54 +0900

    TimeLoggerTest を、縦横それぞれでテストするように修正しました。

--- test/src/jp/dip/oyasirazu/timelogger/TimeLoggerTest.java
+++ test/src/jp/dip/oyasirazu/timelogger/TimeLoggerTest.java
@@ -1,16 +1,18 @@
 package jp.dip.oyasirazu.timelogger;
 
 import jp.dip.oyasirazu.timelogger.OASIZ_TimeLogger;
+import android.app.Activity;
 import android.app.Instrumentation;
 import android.app.Instrumentation.ActivityMonitor;
+import android.content.pm.ActivityInfo;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class TimeLoggerTest extends
         ActivityInstrumentationTestCase2<OASIZ_TimeLogger> {
     
-    private static String PACKAGE_NAME = "jp.dip.oyasirazu.timelogger";
-    private static String DETAIL_VIEW = "jp.dip.oyasirazu.timelogger.DetailView";
+    private static final String PACKAGE_NAME = "jp.dip.oyasirazu.timelogger";
 
     public TimeLoggerTest() {
         super(PACKAGE_NAME, OASIZ_TimeLogger.class);
@@ -24,15 +26,29 @@ public class TimeLoggerTest extends
         super.tearDown();
     }
     
-    public void testShowDetailView() {
-        Instrumentation instrumentation = getInstrumentation();
+    public void testShowDetailViewLand() {
+        testShowDetailView(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+    
+    public void testShowDetailViewPort() {
+        testShowDetailView(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+    
+    private void testShowDetailView(int screenOrientation) {
+        final Instrumentation instrumentation = getInstrumentation();
         
         // 詳細ビューが呼び出されたかを確認するために、 ActivityMonitor を作成
-        ActivityMonitor monitor = new ActivityMonitor(DETAIL_VIEW, null, false);
+        final String detailView = "jp.dip.oyasirazu.timelogger.DetailViewer";
+        final ActivityMonitor monitor = new ActivityMonitor(detailView, null, false);
         instrumentation.addMonitor(monitor);
         
-        // アクティビティと、詳細画面移動ボタンの取得
+        // アクティビティの取得
         final OASIZ_TimeLogger activity = getActivity();
+        
+        // 画面向きの指定
+        activity.setRequestedOrientation(screenOrientation);
+        
+        // 詳細画面移動ボタンの取得
         final Button button = (Button) activity.findViewById(R.id.button_detail_view);
         
         // 詳細画面移動ボタンをクリック
@@ -42,10 +58,18 @@ public class TimeLoggerTest extends
             }
         });
         
-        // クリックイベントが monitor に反映されるのを待つ(?)
-        instrumentation.waitForMonitor(monitor);
+        // クリックイベントが ActivityMonitor に反映されるのを待つ(?)
+        final Activity callee = instrumentation.waitForMonitor(monitor);
         
         // 期待した通り呼び出されているかをチェック
         assertEquals(1, monitor.getHits());
+        
+        // ActivityMonitor の後始末
+        instrumentation.removeMonitor(monitor);
+        
+        // 呼び出した Activity を終了
+        if (callee != null) {
+            callee.finish();
+        }
     }
 }

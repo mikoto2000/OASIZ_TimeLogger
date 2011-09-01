60b1657b9510163218a811c1b6975faf8c7a250f
mikoto <mikoto2000@gmail.com>
2011-09-02 03:44:56 +0900

    作業ログの初期表示についてのテストを修正しました。
    複数アイテムが登録されている場合、作業開始時間の降順に並んでいることを確認するように修正しました。

--- test/src/jp/dip/oyasirazu/timelogger/TimeLoggerTest.java
+++ test/src/jp/dip/oyasirazu/timelogger/TimeLoggerTest.java
@@ -19,7 +19,9 @@ public class TimeLoggerTest extends
         ActivityInstrumentationTestCase2<OASIZ_TimeLogger> {
     
     private static final String PACKAGE_NAME = "jp.dip.oyasirazu.timelogger";
-    private static final String TEST_WORK_NAME = "testAddedWork";
+    private static final String TEST_WORK_NAME1 = "TestWorkName1";
+    private static final String TEST_WORK_NAME2 = "TestWorkName2";
+    private static final String TEST_WORK_NAME3 = "TestWorkName3";
     private static final long START_TIME = 10000;
     private static final long END_TIME = 12000;
     private static final double DELTA = 1000.0;
@@ -30,9 +32,11 @@ public class TimeLoggerTest extends
     
     protected void setUp() throws Exception {
         super.setUp();
-        if (getName().equals("testAddedWork")) {
+        if (getName().equals("testFirstDisplayWork")) {
             WorkLogDatabase wld = new WorkLogDatabase(getInstrumentation().getTargetContext(), new SimpleDateFormat("yyyy/MM/dd HH:mm:ss"));
-            wld.add(new Work(TEST_WORK_NAME, new Date(START_TIME), new Date(END_TIME)));
+            wld.add(new Work(TEST_WORK_NAME1, new Date(START_TIME), new Date(END_TIME)));
+            wld.add(new Work(TEST_WORK_NAME2, new Date(START_TIME + 1), new Date(END_TIME + 1)));
+            wld.add(new Work(TEST_WORK_NAME3, new Date(START_TIME + 2), new Date(END_TIME + 2)));
             wld.close();
         }
     }
@@ -83,7 +87,7 @@ public class TimeLoggerTest extends
         instrumentation.waitForIdleSync();
         
         // 文字入力
-        final String workName = TEST_WORK_NAME;
+        final String workName = TEST_WORK_NAME1;
         instrumentation.sendStringSync(workName);
         
         // UI スレッドと同期
@@ -145,7 +149,6 @@ public class TimeLoggerTest extends
      * アクティビティ起動時に、データベースにデータが存在する場合は、 ListView にログを表示する。<br />
      * 正しく表示されるかを確認する。<br />
      * setUp メソッドで、予めデータベースにデータを追加してある。<br />
-     * TODO:データが高順に格納されているかを確認する。
      */
     public void testFirstDisplayWork() {
         // アクティビティの取得
@@ -155,18 +158,26 @@ public class TimeLoggerTest extends
         final ListView listView = (ListView) activity.findViewById(R.id.log_view);
         final Adapter adapter = listView.getAdapter();
         
-        // サイズは 1 のはず
-        assertEquals(1, adapter.getCount());
+        // サイズは 3 のはず
+        assertEquals(3, adapter.getCount());
         
-        // adapter に格納されている Work 内容が正しいかをチェック
-        final Work work = (Work)adapter.getItem(0);
+        // adapter に格納されている Work 内容が正しいかをチェック(startTime 降順に格納されているはず)
+        final Work work1 = (Work)adapter.getItem(0);
+        final Work work2 = (Work)adapter.getItem(1);
+        final Work work3 = (Work)adapter.getItem(2);
         
         // 作業名のチェック
-        assertEquals(TEST_WORK_NAME, work.getName());
+        assertEquals(TEST_WORK_NAME3, work3.getName());
+        assertEquals(TEST_WORK_NAME2, work2.getName());
+        assertEquals(TEST_WORK_NAME1, work1.getName());
         
         // 開始・終了時刻のチェック(1 秒以内の誤差は許す)
-        assertEquals((double)START_TIME, (double)work.getStartDate().getTime(), DELTA);
-        assertEquals((double)END_TIME, (double)work.getEndDate().getTime(), DELTA);
+        assertEquals((double)START_TIME + 2, (double)work1.getStartDate().getTime(), DELTA);
+        assertEquals((double)END_TIME + 2, (double)work1.getEndDate().getTime(), DELTA);
+        assertEquals((double)START_TIME + 1, (double)work2.getStartDate().getTime(), DELTA);
+        assertEquals((double)END_TIME + 1, (double)work2.getEndDate().getTime(), DELTA);
+        assertEquals((double)START_TIME, (double)work3.getStartDate().getTime(), DELTA);
+        assertEquals((double)END_TIME, (double)work3.getEndDate().getTime(), DELTA);
     }
     
     /**
